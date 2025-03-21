14e47c9b57a23ce8a30109f77129126e02105361
Nuran Ramis <nuranrms@gmail.com>
2025-03-21 13:44:03 +0100

    delete note

--- src/components/App.jsx
+++ src/components/App.jsx
@@ -6,7 +6,6 @@ import CreateArea from "./CreateArea";
 
 
 function App() {
-
   const [notes, setNote] = useState([]);
 
   function addNote(newNote) {
@@ -14,18 +13,30 @@ function App() {
       return [...prevNotes, newNote];
     })
   }
+
+  function deleteNote(id) {
+    setNote(prevNotes => {
+      return prevNotes.filter((noteItem, index) => {
+        return index !== id;
+      });
+    });
+  }
+
   return (
     <div>
       <Header />
       <CreateArea onAdd={addNote}/>
-      {notes.map((noteItem, index) => (
+      {notes.map((noteItem, index) => {
+        return (
         <Note
           key={index}
           id={index}
           title={noteItem.title}
           content={noteItem.content}
+          onDelete={deleteNote}
         />
-      ))}
+      );
+    })}
       <Footer />
     </div>
   );
